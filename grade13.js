cc67111046e2215a7e349b9b057ad97c9f79882f
Evangeline <Evangeline@appledeMacBook.local>
2019-03-27 22:26:48 -0400

    circle

--- grade13.js
+++ grade13.js
@@ -1,6 +1,6 @@
 var grade=d3.json("penguins/classData.json");
-var w=600;
-var h=400;
+var w=500;
+var h=500;
 var margin={
   left:20,
   right:20,
@@ -27,11 +27,56 @@ grade.then(function(data){
   var xAxis=d3.axisBottom(xScale);
   var yAxis=d3.axisLeft(yScale);
   svg.append("g")
-     .classed(".xAxis",true)
+     .classed("xAxis",true)
      .call(xAxis)
      .attr("transform","translate("+margin.left+","+(margin.top+h)+")");
   svg.append("g")
-     .classed(".yAxis",true)
+     .classed("yAxis",true)
      .call(yAxis)
      .attr("transform","translate("+(margin.left+margin.right)+",0)");
+     svg.selectAll("circle")
+        .data(data[12].homework)
+        .enter()
+        .append("circle")
+        .attr("cx",function(d){return xScale(d.day,10);})
+        .attr("cy",function(d){return yScale(d.grade,10);})
+        .attr("r","5")
+        .attr("fill","black");
+},function(err){console.log(err);})
+/////////////////////////////////////////////////
+grade.then(function(data){
+  var xScale=d3.scaleLinear()
+               .domain([0,40])
+               .range([margin.left+margin.right-10,h+margin.left]);
+  var yScale=d3.scaleLinear()
+               .domain([0,10])
+               .range([h+margin.top,margin.top]);
+  var svg=d3.select("#line2")
+            .attr("width",w+margin.left+margin.right)
+            .attr("height",h+margin.top+margin.bottom);
+  var line=d3.line()
+             .x(function(d){return xScale(d.day,10);})
+             .y(function(d){return yScale(d.grade,10);});
+  svg.append("path")
+     .datum(data[12].quizes)
+     .classed("line",true)
+     .attr("d",line);
+  var xAxis=d3.axisBottom(xScale);
+  var yAxis=d3.axisLeft(yScale);
+  svg.append("g")
+     .classed("xAxis",true)
+     .call(xAxis)
+     .attr("transform","translate("+(margin.left-10)+","+(margin.top+h)+")");
+  svg.append("g")
+     .classed("yAxis",true)
+     .call(yAxis)
+     .attr("transform","translate("+(margin.left+margin.right)+",0)");
+     svg.selectAll("circle")
+        .data(data[12].quizes)
+        .enter()
+        .append("circle")
+        .attr("cx",function(d){return xScale(d.day,10);})
+        .attr("cy",function(d){return yScale(d.grade,10);})
+        .attr("r","5")
+        .attr("fill","black");
 },function(err){console.log(err);})
