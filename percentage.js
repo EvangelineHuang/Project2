39c5c9c6c80c4a3cd12f0f761769ee7853fe0806
WynnieSun <wynniesun@WynnieSundeMacBook-Air.local>
2019-03-31 22:03:48 -0400

    pie chart

---
+++ percentage.js
@@ -0,0 +1,67 @@
+var w = 300;
+var h = 300;
+var r = Math.min(w,h)/2;
+
+var color = d3.scaleOrdinal(["#DD6E42","#E8DAB2","#6E9075","#C0D6DF","#EAEAEA","#5D737E"]);
+
+var data = [{"label":"Quizzes", "value":15},
+            {"label":"Homework", "value":15},
+            {"label":"Test-1", "value":20},
+            {"label":"Test-2", "value":20},
+            {"label":"Final", "value":30}];
+            console.log(data)
+
+    var plot = d3.select("body")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h)
+                .append("g")
+                .attr("transform", "translate(" + r + "," + r + ")")
+
+    var arc = d3.arc()
+                .innerRadius(0)
+                .outerRadius(r);
+
+    var pie = d3.pie()
+                .sort(null)
+                .value(function(d) { return d.value; })(data);
+
+    var arcs = plot.selectAll("arc")
+                   .data(pie)
+                   .enter()
+                   .append("g")
+                   .attr("class", "arc");
+
+        arcs.append("path")
+            .attr("d", arc)
+            .attr("fill",function(d,i){return color(i)})
+        arcs.append("text")
+            .attr("transform", function(d){
+                innerRadius = 0;
+                outerRadius = r;
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("text-anchor", "middle")
+            .text(function(d, i) { return data[i].label; })
+            .attr("x", 0)
+            .attr("y", "0em");
+        arcs.append("text")
+            .attr("transform", function(d){
+                    innerRadius = 0;
+                    outerRadius = r;
+                    return "translate(" + arc.centroid(d) + ")";
+                })
+            .attr("text-anchor", "middle")
+            .text(function(d, i) { return data[i].value; })
+            .attr("x", -5)
+            .attr("y", "1.4em");
+        arcs.append("text")
+            .attr("transform", function(d){
+                innerRadius = 0;
+                outerRadius = r;
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("text-anchor", "middle")
+            .text(function(d, i) { return "%"; })
+            .attr("x", 13)
+            .attr("y", "1.4em");
