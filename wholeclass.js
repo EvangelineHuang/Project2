79c62cab75bbe4aa87894f6ef0ed480487dca8b2
Evangeline <Evangeline@appledeMacBook.local>
2019-04-03 17:00:23 -0400

    fuck

--- wholeclass.js
+++ wholeclass.js
@@ -1,4 +1,8 @@
 var grade=d3.json("3Grades.json");
+var bgrade=d3.json("penguins/classData.json");
+var change=d3.csv("GradesChange.csv")
+var width=500;
+var height=500;
 var padding=2;
 var margin={
   left:40,
@@ -6,35 +10,164 @@ var margin={
   top:20,
   bottom:20
 }
-//ranking changes
-var change=[{name:"bookworm",test2:-2,final:-3},
-{name:"crafty",test2:-6,final:6},
-{name:"cyclist",test2:1,final:-1},
-{name:"drunken",test2:5,final:5},
-{name:"Easter",test2:8,final:-1},
-{name:"ebook",test2:-1,final:0},
-{name:"Farmer",test2:-12,final:6},
-{name:"gentleman",test2:5,final:4},
-{name:"judo",test2:-1,final:1},
-{name:"moana",test2:3,final:-9},
-{name:"painter",test2:8,final:10},
-{name:"grill",test2:-1,final:0},
-{name:"pharaoh",test2:-6,final:-1},
-{name:"pilot",test2:1,final:-4},
-{name:"Pinga",test2:-11,final:6},
-{name:"pixie",test2:4,final:5},
-{name:"sailor",test2:0,final:5},
-{name:"santa",test2:0,final:-3},
-{name:"tauch",test2:7,final:-15},
-{name:"tux",test2:-1,final:2},
-{name:"valentine-ocal",test2:0,final:-4},
-{name:"valentine",test2:3,final:-5},
-{name:"wizard",test2:-4,final:-4}]
-//console.log(change)
+function homework(data,ind){
+  console.log(data)
+  var xScale=d3.scaleLinear()
+               .domain([0,40])
+               .range([margin.left,height+margin.left]);
+  var yScale=d3.scaleLinear()
+               .domain([0,50])
+               .range([height+margin.top,margin.top]);
+  var svg=d3.select("#line")
+            .attr("width",width+margin.left+margin.right)
+            .attr("height",height+margin.top+margin.bottom);
+  var line=d3.line()
+             .x(function(d){return xScale(d.day);})
+             .y(function(d){return yScale(d.grade);});
 
-var graph=function(data,datum,rank){
-  var width=500;
-  var height=500;
+  svg.append("path")
+     .datum(data[ind].homework)
+     .classed("line",true)
+     .attr("d",line);
+  var xAxis=d3.axisBottom(xScale);
+  var yAxis=d3.axisLeft(yScale);
+  svg.append("g")
+     .classed("xAxis",true)
+     .call(xAxis)
+     .attr("transform","translate("+margin.left/2+","+(margin.top+height)+")");
+  svg.append("g")
+     .classed("yAxis",true)
+     .call(yAxis)
+     .attr("transform","translate("+(margin.left+margin.right)+",0)");
+  svg.selectAll("circle")
+     .data(data[ind].homework)
+     .enter()
+     .append("circle")
+     .attr("cx",function(d){return xScale(d.day);})
+     .attr("cy",function(d){return yScale(d.grade);})
+     .attr("r","5")
+     .attr("fill","black")
+     .on("mouseover",function(d){
+       d3.select("#tooltip1")
+         .style("left",width+margin.left+margin.right+300)
+         .style("top",20)
+         .select("#grade1")
+         .text("Grade: "+d.grade);
+        d3.select("#day1")
+          .text("Day: "+d.day)
+      d3.select("#tooltip1").classed("hidden",false);
+     })
+     .on("mouseout",function(){
+      d3.select("#tooltip1").classed("hidden",true);
+     });
+}
+function quize(data,ind){
+  var xScale=d3.scaleLinear()
+               .domain([0,40])
+               .range([margin.left+margin.right-10,width+margin.left]);
+  var yScale=d3.scaleLinear()
+               .domain([0,10])
+               .range([height+margin.top,margin.top]);
+  var svg=d3.select("#line2")
+            .attr("width",width+margin.left+margin.right)
+            .attr("height",height+margin.top+margin.bottom);
+  var line=d3.line()
+             .x(function(d){return xScale(d.day);})
+             .y(function(d){return yScale(d.grade);});
+  svg.append("path")
+     .datum(data[ind].quizes)
+     .classed("line",true)
+     .attr("d",line);
+  var xAxis=d3.axisBottom(xScale);
+  var yAxis=d3.axisLeft(yScale);
+  svg.append("g")
+     .classed("xAxis",true)
+     .call(xAxis)
+     .attr("transform","translate(10,"+(margin.top+height)+")");
+  svg.append("g")
+     .classed("yAxis",true)
+     .call(yAxis)
+     .attr("transform","translate("+(margin.left+margin.right)+",0)");
+     svg.selectAll("circle")
+        .data(data[ind].quizes)
+        .enter()
+        .append("circle")
+        .attr("cx",function(d){return xScale(d.day);})
+        .attr("cy",function(d){return yScale(d.grade);})
+        .attr("r","5")
+        .attr("fill","black")
+        .on("mouseover",function(d){
+          d3.select("#tooltip1")
+            .style("left",width+margin.left+margin.right+300)
+            .style("top",20)
+            .select("#grade1")
+            .text("Garde: "+d.grade);
+           d3.select("#day1")
+             .text("Day: "+d.day)
+         d3.select("#tooltip1").classed("hidden",false);
+        })
+        .on("mouseout",function(){
+         d3.select("#tooltip1").classed("hidden",true);
+        });
+}
+////////////////////////////////////////////////////
+function test(data, ind){
+  var xScale=d3.scaleLinear()
+               .domain([0,2])
+               .range([margin.left+margin.right-10,width+margin.left]);
+  var yScale=d3.scaleLinear()
+               .domain([0,100])
+               .range([height+margin.top,margin.top]);
+  var svg=d3.select("#line3")
+            .attr("width",width+margin.left+margin.right)
+            .attr("height",height+margin.top+margin.bottom);
+  var line=d3.line()
+             .x(function(d,i){return xScale(i);})
+             .y(function(d){return yScale(d);});
+
+  svg.append("path")
+     .datum(data[ind].test)
+     .classed("line",true)
+     .attr("d",line);
+  var xAxis=d3.axisBottom(xScale);
+  var yAxis=d3.axisLeft(yScale);
+  svg.append("g")
+     .classed("xAxis",true)
+     .call(xAxis)
+     .attr("transform","translate(0,"+(margin.top+height)+")");
+  svg.append("g")
+     .classed("yAxis",true)
+     .call(yAxis)
+     .attr("transform","translate("+(margin.right+30)+",0)");
+     svg.selectAll("circle")
+        .data(data[ind].test)
+        .enter()
+        .append("circle")
+        .attr("cx",function(d,i){return xScale(i);})
+        .attr("cy",function(d){return yScale(d);})
+        .attr("r","5")
+        .on("mouseover",function(d,i){
+          d3.select("#tooltip3")
+            .style("left",width+margin.left+margin.right+300)
+            .style("top",20)
+            .select("#Test")
+            .text("Test:"+d);
+           d3.select("#name")
+             .text(function(){
+               if(i==2)
+               {return "Name: Final";}
+               else
+               {return "Name: Test"+(i+1)}
+             })
+         d3.select("#tooltip3").classed("hidden",false);
+        })
+        .on("mouseout",function(){
+         d3.select("#tooltip3").classed("hidden",true);
+        });
+}
+/////////////////////////////////////////////////////
+var graph=function(data,datum,rank,dt){
+
   var color=["#ccebb4","#bfe6b5","#b4e2b6","#a8ddb7","#9cd8b8","#91d4b9","#86d0bb","#71c8bd","#66c3bf","#59bec0","#4eb9c1","#45b3c2","#3fafc2","#34a7c2","#2c9dc0","#2897bf","#238cbc","#217fb7","#2076b3","#216aae","#225ea8","#2353a3","#23499d"]
   var xScale=d3.scaleLinear()
                .domain([1,24])
@@ -60,8 +193,7 @@ var graph=function(data,datum,rank){
      })
      .attr("class",function(d,i){
        return datum[i].name;})
-     .on("mouseover",function(){
-
+     .on("mouseover",function(d,i){
        var name=d3.select(this).attr("class");
        d3.select("p."+name)
          .style("background-color","orange")
@@ -71,12 +203,46 @@ var graph=function(data,datum,rank){
        d3.selectAll("p")
          .style("background-color","transparent")
      })
-     .on("click",function(d,i)
-     { var c=d3.select(this);
-       d3.select(".info")
-       .text(function(){
-         return "Name: "+change[i].name+"; Grade :"+datum[i].test[0];
-       })})
+     .on("click",function(d,i){
+       d3.select("#body").remove();
+       var body=d3.select("body")
+         .append("div")
+         .attr("id","body")
+      body.append("svg")
+          .attr("id","line");
+      body.append("svg")
+          .attr("id","line2");
+      body.append("svg")
+          .attr("id","line3");
+      var t1=body.append("div")
+          .attr("id","tooltip1")
+          .classed("hidden",true);
+          t1.append("span")
+            .attr("id","grade1");
+          t1.append("br")
+            t1.append("span")
+              .attr("id","day1")
+      var t2=body.append("div")
+          .attr("id","tooltip2")
+          .classed("hidden",true);
+          t2.append("span")
+            .attr("id","grade2");
+          t2.append("br")
+            t2.append("span")
+              .attr("id","day2")
+      var t3=body.append("div")
+          .attr("id","tooltip3")
+          .classed("hidden",true);
+          t3.append("span")
+            .attr("id","Test");
+          t3.append("br")
+            t3.append("span")
+              .attr("id","Name")
+       homework(dt,i);
+       quize(dt,i);
+      test(datum,i);
+
+     })
 
      svg.append("g")
         .classed("testxAxis",true)
@@ -109,13 +275,15 @@ var graph=function(data,datum,rank){
         d3.selectAll("rect")
           .attr("stroke","none")
       })
+
+
       d3.select("#ranking")
         .text("Test 1")
 
 
 }
 ///////////////////////////////////////////
-var changegraph=function(data,datum,rank,c,s,ind){
+var changegraph=function(data,datum,rank){
   var width=500;
   var height=500;
   var xScale=d3.scaleLinear()
@@ -129,7 +297,6 @@ var changegraph=function(data,datum,rank,c,s,ind){
     .attr("height",height+margin.top+margin.bottom);
   var xAxis=d3.axisBottom(xScale);
   var yAxis=d3.axisLeft(yScale);
-console.log(c)
   svg.selectAll("rect")
      .data(data)
      .transition()
@@ -138,24 +305,7 @@ console.log(c)
      .attr("width",width/23-padding)
      .attr("height",function(d){return height-yScale(d)})
      .attr("class",function(d,i){
-       return datum[i].name;})
-    d3.selectAll("rect")
-      .on("click",function(d,i)
-       {
-            d3.select(".info")
-             .text(function(){
-               if(s){
-                 return "Name: "+datum[i].name+"; Grade: "+datum[i].test[0]+";";
-               }
-               else{
-               if(c[i].grade>0)
-               {return "Name: "+c[i].name+"; Grade: "+datum[i].test[ind]+"; Improve "+Math.abs(c[i].grade)+" spot";}
-               else if(c[i].grade<0)
-               {return "Name: "+c[i].name+"; Grade: "+datum[i].test[ind]+"; Drop "+Math.abs(c[i].grade)+" spot";}
-               else{
-                 return "Name: "+c[i].name+"; Grade: "+datum[i].test[ind]+"; No Change From Last Test";}
-
-             }})})
+       return datum[i].name;});
         d3.selectAll("p")
           .data(rank)
           .attr("class",function(d){
@@ -185,65 +335,61 @@ var frank=[{name:"tux",grade:"97"},{name:"sailor",grade:"91"},{name:"crafty",gra
           {name:"pixie",grade:"62"},{name:"Farmer",grade:"60"},{name:"Pinga",grade:"59"},{name:"valentine-ocal",grade:"58"},{name:"santa",grade:"57"},
           {name:"bookworm",grade:"55"},{name:"tauch",grade:"54"},{name:"cyclist",grade:"46"},{name:"valentine",grade:"46"},{name:"ebook",grade:"44"},
           {name:"judo",grade:"29"},{name:"pharaoh",grade:"19"},{name:"grill",grade:"14"}]
-var t2=change.map(function(d,i){
-  return {
-    name:change[i].name,
-    grade:change[i].test2
-  }
-})
-var f=change.map(function(d,i){
-  return {
-    name:change[i].name,
-    grade:change[i].final
-  }
-})
-grade.then(function(data){
-  var test1=data.map(function(d){
-    return d.test[0];
-  });
-    graph(test1,data,rank1);
 
+bgrade.then(function(data){
+
+  var test1=data.map(function(d){
+    return d.test[0].grade;
+  });
+  grade.then(function(d){
+    graph(test1,d,rank1,data);},function(err){console.log(err);});
 },function(err){console.log(err);});
 
-d3.select("body")
+d3.select("#body")
   .append("button")
   .classed("test1",true)
   .on("click",function(){
-    grade.then(function(data){
+    bgrade.then(function(data){
       var test1=data.map(function(d){
-        return d.test[0];
+        return d.test[0].grade;
       });
-        changegraph(test1,data,rank1,f,true,null);
+      grade.then(function(d){
+        changegraph(test1,d,rank1,data);
+        },function(err){console.log(err);});
         d3.select("#ranking")
           .text("Test 1")
     },function(err){console.log(err);});
   })
   .text("Test 1");
 
-d3.select("body")
+d3.select("#body")
   .append("button")
   .classed("test2",true)
   .on("click",function(){
-    grade.then(function(data){
+    bgrade.then(function(data){
       var test2=data.map(function(d){
-        return d.test[1];
+        return d.test[1].grade;
       });
-        changegraph(test2,data,rank2,t2,false,1);
+      grade.then(function(d){
+        changegraph(test2,d,rank2,data);
+        },function(err){console.log(err);});
         d3.select("#ranking")
           .text("Test 2")
     },function(err){console.log(err);});
   })
   .text("Test 2");
 
-  d3.select("body")
+  d3.select("#body")
     .append("button")
     .classed("final",true)
     .on("click",function(){
-      grade.then(function(data){
+      bgrade.then(function(data){
         var final=data.map(function(d){
-          return d.test[2];
+          return d.test[2].grade;
         });
-          changegraph(final,data,frank,f,false,2);
+        grade.then(function(d){
+          changegraph(final,d,frank,data);
+          },function(err){console.log(err);});
           d3.select("#ranking")
             .text("Final")
 
