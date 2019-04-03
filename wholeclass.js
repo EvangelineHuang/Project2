fa783d29ecb21857ff70e368cf4f3455870c5ba5
Evangeline <Evangeline@appledeMacBook.local>
2019-04-02 22:23:21 -0400

    parameter

--- wholeclass.js
+++ wholeclass.js
@@ -70,7 +70,13 @@ var graph=function(data,datum,rank){
 
        d3.selectAll("p")
          .style("background-color","transparent")
-     });
+     })
+     .on("click",function(d,i)
+     { var c=d3.select(this);
+       d3.select(".info")
+       .text(function(){
+         return "Name: "+change[i].name+"; Grade :"+datum[i].test[0];
+       })})
 
      svg.append("g")
         .classed("testxAxis",true)
@@ -102,13 +108,14 @@ var graph=function(data,datum,rank){
         d3.selectAll("p").style("background-color","transparent")
         d3.selectAll("rect")
           .attr("stroke","none")
-      });
+      })
       d3.select("#ranking")
         .text("Test 1")
 
+
 }
 ///////////////////////////////////////////
-var changegraph=function(data,datum,rank){
+var changegraph=function(data,datum,rank,c,s,ind){
   var width=500;
   var height=500;
   var xScale=d3.scaleLinear()
@@ -122,7 +129,7 @@ var changegraph=function(data,datum,rank){
     .attr("height",height+margin.top+margin.bottom);
   var xAxis=d3.axisBottom(xScale);
   var yAxis=d3.axisLeft(yScale);
-
+console.log(c)
   svg.selectAll("rect")
      .data(data)
      .transition()
@@ -132,6 +139,23 @@ var changegraph=function(data,datum,rank){
      .attr("height",function(d){return height-yScale(d)})
      .attr("class",function(d,i){
        return datum[i].name;})
+    d3.selectAll("rect")
+      .on("click",function(d,i)
+       {
+            d3.select(".info")
+             .text(function(){
+               if(s){
+                 return "Name: "+datum[i].name+"; Grade: "+datum[i].test[0]+";";
+               }
+               else{
+               if(c[i].grade>0)
+               {return "Name: "+c[i].name+"; Grade: "+datum[i].test[ind]+"; Improve "+Math.abs(c[i].grade)+" spot";}
+               else if(c[i].grade<0)
+               {return "Name: "+c[i].name+"; Grade: "+datum[i].test[ind]+"; Drop "+Math.abs(c[i].grade)+" spot";}
+               else{
+                 return "Name: "+c[i].name+"; Grade: "+datum[i].test[ind]+"; No Change From Last Test";}
+
+             }})})
         d3.selectAll("p")
           .data(rank)
           .attr("class",function(d){
@@ -141,24 +165,38 @@ var changegraph=function(data,datum,rank){
             return i+1+". Name: "+d.name+", Grade: "+d.grade;
           })
 }
-///////////////////////////////
+//ranking of test1
 var rank1=[{name:"wizard",grade:"100"},{name:"tux",	grade:"91"},{name:"crafty",grade:"90"},
            {name:"moana",grade:"89"},{name:"pilot",grade:"85"},{name:"Farmer",grade:"76"},{name:"sailor",grade:"74"},
            {name:"Pinga",grade:"72"},{name:"tauch",grade:"71"},{name:"valentine-ocal",grade:"70"},{name:"bookworm",grade:"68"},
            {name:"santa",grade:"68"},{name:"gentleman",grade:"67"},{name:"Easter",grade:"65"},{name:"pharaoh",grade:"63"},
            {name:"drunken",grade:"59"},{name:"valentine",grade:"54"},{name:"cyclist",grade:"49"},{name:"ebook",grade:"48"},
            {name:"pixie",grade:"47"},{name:"judo",grade:"45"},{name:"grill",grade:"44"},{name:"painter",grade:"43"}];
+//ranking of test2
 var rank2=[{name:"moana",grade:"100"},{name:"tauch",grade:"90"},{name:"tux",grade:"88"},{name:"pilot",grade:"86"},
           {name:"wizard",grade:"86"},{name:"Easter",grade:"83"},{name:"sailor",grade:"80"},{name:"gentleman",grade:"71"},
           {name:"crafty",	grade:"70"},{name:"valentine-ocal",grade:"70"},{name:	"drunken",grade:"68"},{name:"santa",grade:"66"},
           {name:"bookworm",	grade:"65"},{name:"valentine",grade:"62"},{name:"painter",grade:"61"},{name:"pixie",grade:"57"},
           {name:"cyclist",	grade:"56"},{name:"Farmer",grade:"54"},{name:"Pinga",grade:"52"},{name:"ebook",grade:"33"},
           {name:"pharaoh",grade:"28"},{name:"judo",grade:"19"},{name:"grill",grade:"14"}]
+//ranking of final
 var frank=[{name:"tux",grade:"97"},{name:"sailor",grade:"91"},{name:"crafty",grade:"77"},{name:"gentleman",grade:"75"},{name:"painter",grade:"75"},
           {name:"drunken",grade:"74"},{name:"Easter",grade:"71"},{name:"pilot",grade:"71"},{name:"wizard",grade:"68"},{name:"moana",grade:"67"},
           {name:"pixie",grade:"62"},{name:"Farmer",grade:"60"},{name:"Pinga",grade:"59"},{name:"valentine-ocal",grade:"58"},{name:"santa",grade:"57"},
           {name:"bookworm",grade:"55"},{name:"tauch",grade:"54"},{name:"cyclist",grade:"46"},{name:"valentine",grade:"46"},{name:"ebook",grade:"44"},
           {name:"judo",grade:"29"},{name:"pharaoh",grade:"19"},{name:"grill",grade:"14"}]
+var t2=change.map(function(d,i){
+  return {
+    name:change[i].name,
+    grade:change[i].test2
+  }
+})
+var f=change.map(function(d,i){
+  return {
+    name:change[i].name,
+    grade:change[i].final
+  }
+})
 grade.then(function(data){
   var test1=data.map(function(d){
     return d.test[0];
@@ -175,7 +213,7 @@ d3.select("body")
       var test1=data.map(function(d){
         return d.test[0];
       });
-        changegraph(test1,data,rank1);
+        changegraph(test1,data,rank1,f,true,null);
         d3.select("#ranking")
           .text("Test 1")
     },function(err){console.log(err);});
@@ -190,7 +228,7 @@ d3.select("body")
       var test2=data.map(function(d){
         return d.test[1];
       });
-        changegraph(test2,data,rank2);
+        changegraph(test2,data,rank2,t2,false,1);
         d3.select("#ranking")
           .text("Test 2")
     },function(err){console.log(err);});
@@ -205,9 +243,10 @@ d3.select("body")
         var final=data.map(function(d){
           return d.test[2];
         });
-          changegraph(final,data,frank);
+          changegraph(final,data,frank,f,false,2);
           d3.select("#ranking")
             .text("Final")
+
       },function(err){console.log(err);});
     })
     .text("Final");
